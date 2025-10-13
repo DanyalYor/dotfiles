445526dcd054d408dff9dddb6dfe9741216825fb
Danyal <skoledanyal@gmail.com>
2025-10-13 14:27:10 +0200

    Update

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,59 +1,24 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>op",
+        function()
+          local peek = require "peek"
+          if peek.is_open() then
+            peek.close()
+          else
+            peek.open()
+          end
+        end,
+        desc = "Peek (Markdown Preview)",
+      },
     },
-
-    {
-        'rmagatti/auto-session',
-        lazy = false,
-
-        ---enables autocomplete for opts
-        ---@module "auto-session"
-        ---@type AutoSession.Config
-        opts = {
-            suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-            -- log_level = 'debug',
-        }
-    },
-
-    {
-        {
-            "folke/trouble.nvim",
-            opts = {}, -- for default options, refer to the configuration section for custom setup.
-            cmd = "Trouble",
-            keys = {
-                {
-                    "<leader>xx",
-                    "<cmd>Trouble diagnostics toggle<cr>",
-                    desc = "Diagnostics (Trouble)",
-                },
-                {
-                    "<leader>xX",
-                    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                    desc = "Buffer Diagnostics (Trouble)",
-                },
-                {
-                    "<leader>cs",
-                    "<cmd>Trouble symbols toggle focus=false<cr>",
-                    desc = "Symbols (Trouble)",
-                },
-                {
-                    "<leader>cl",
-                    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                    desc = "LSP Definitions / references / ... (Trouble)",
-                },
-                {
-                    "<leader>xL",
-                    "<cmd>Trouble loclist toggle<cr>",
-                    desc = "Location List (Trouble)",
-                },
-                {
-                    "<leader>xQ",
-                    "<cmd>Trouble qflist toggle<cr>",
-                    desc = "Quickfix List (Trouble)",
-                },
-            },
-        }
-    }
+    opts = { theme = "dark", app = "browser" },
+  },
+  -- Make sure these optional dependencies are included
+  "rcarriga/nvim-notify",
+  "stevearc/dressing.nvim",
 }
